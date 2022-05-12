6d34dfaaf8e7ca2bb276a65a0e7b6803d5ae2ec1
remoppou <voronezhskij_n_m@sc.vsu.ru>
2022-05-12 13:02:32 +0300

    Микро добавки по коду

--- src/ru/vsu/cs/course1/sort/SmoothSort.java
+++ src/ru/vsu/cs/course1/sort/SmoothSort.java
@@ -7,27 +7,24 @@ public class SmoothSort<T>{
 
     private static int[] leonardoMemo = { 1, 1 };
 
-    public static int leonardo(int n) {       //Вычисление чисел леонардо
+    public static int leonardo(int n) {   //Массив чисел леонардо
         if (n < leonardoMemo.length) {
             if (leonardoMemo[n] != 0) {
                 return leonardoMemo[n];
             }
         } else {
             int newLength = leonardoMemo.length * 2;
-            leonardoMemo = Arrays.copyOf(leonardoMemo,
-                    (newLength > n) ? newLength : (n + 1));
+            leonardoMemo = Arrays.copyOf(leonardoMemo, (newLength > n) ? newLength : (n + 1));
         }
-        return leonardoMemo[n] = leonardo(n - 1)
-                + leonardo(n - 2) + 1;
+        return leonardoMemo[n] = leonardo(n - 1) + leonardo(n - 2) + 1;
     }
 
-    private static <T extends Comparable<? super T>> void sift(T[] data, int head, int exp) {   //просейка
+    private static <T extends Comparable<? super T>> void sift(T[] data, int head, int exp) {  //просейка и сравнение элементов(расстановка элементов по местам)
         T t = data[head];
         while (exp > 1) {
             int r = head - 1;
             int l = head - 1 - leonardo(exp - 2);
-            if (t.compareTo(data[l]) >= 0 &&
-                    t.compareTo(data[r]) >= 0) {
+            if (t.compareTo(data[l]) >= 0 && t.compareTo(data[r]) >= 0) {
                 break;
             }
             if (data[l].compareTo(data[r]) >= 0) {
@@ -45,7 +42,7 @@ public class SmoothSort<T>{
 
 
 
-    private static <T extends Comparable<? super T>> void arrange(T[] v, int head, long frac, int exp) {    //Расставление
+    private static <T extends Comparable<? super T>> void arrange(T[] v, int head, long frac, int exp) {  //Расставление со сравнением, идет что то типа формирования кучи
         T t = v[head];
         while (frac > 1) {
             int next = head - leonardo(exp);
@@ -55,8 +52,7 @@ public class SmoothSort<T>{
             if (exp > 1) {
                 int r = head - 1;
                 int l = head - 1 - leonardo(exp - 2);
-                if (v[l].compareTo(v[next]) >= 0
-                        || v[r].compareTo(v[next]) >= 0) {
+                if (v[l].compareTo(v[next]) >= 0 || v[r].compareTo(v[next]) >= 0) {
                     break;
                 }
             }
@@ -71,13 +67,14 @@ public class SmoothSort<T>{
     }
 
     public static <T extends Comparable<? super T>> void sort(T[] data) {
-        if (data.length == 0) return;
-        int head = 0;
-        long frac = 0;
-        int exp = 0;
+        if (data.length == 0) return;  //size
+        int head = 0;     //num el
+        long frac = 0;      //важная переменная, но не понял ее роли
+        int exp = 0;       //важная переменная, отвечает за сравнение
+        //Проходим все элементы (формируется последовательность куч)
         while (head < data.length) {
-            if ((frac & 3) == 3) {
-                frac >>>= 2;
+            if ((frac & 3) == 3) {     //dont understand why frac&3 ==3
+                frac >>>= 2;        //побитовый сдвиг, тоже их много и не особо понятно зачем они нужны
                 exp += 2;
             } else if (exp > 1) {
                 frac <<= exp - 1;
@@ -87,14 +84,14 @@ public class SmoothSort<T>{
                 exp ^= 1;
             }
             frac++;
-            if (exp > 0 && data.length - head - 1 <
-                    leonardo(exp - 1) + 1) {
+            if (exp > 0 && data.length - head - 1 < leonardo(exp - 1) + 1) {
                 arrange(data, head, frac, exp);
             }
             sift(data, head, exp);
             head++;
         }
-        arrange(data, head - 1, frac, exp);
+        arrange(data, head - 1, frac, exp); //удаление самого правого
+        //удаление до самого левого с правого
         while (head > 0) {
             head--;
             if (exp > 1) {
@@ -117,19 +114,5 @@ public class SmoothSort<T>{
      * @param <T> Сравнимый тип элементов массива
      * @param data Сортируемый массив типа T
      */
-//    public static <T extends Comparable<T>> void sort(T[] data) {
-//        /*
-//        class TempComparator implements Comparator<T> {
-//            @Override
-//            public int compare(T a, T b) {
-//                return a.compareTo(b);
-//            }
-//        }
-//        sort(data, new TempComparator());
-//        */
-//        /*
-//        sort(data, (a, b) -> a.compareTo(b));
-//        */
-//        sort(data, Comparable::compareTo);
-//    }
+
 }
